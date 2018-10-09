9ad28435afac3cbdac3f1a9d08f9ecf8fcaacfde
Filippo Ferri <hello@filippoferri.co>
2018-10-09 14:50:06 +0200

    logo updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => (
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <figure className="image">
-            <img src={logo} alt="Guadagnare col Matched Betting" style={{ width: "88px" }}/>
+            <img className="logo" src={logo} alt="Guadagnare col Matched Betting"/>
           </figure>
         </Link>
       </div>
